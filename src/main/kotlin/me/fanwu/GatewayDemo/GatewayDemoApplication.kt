6a1cb39b0e508096b4aa270425db14cae710f517
Fan Wu <fanwu@pm.me>
2022-04-20 16:20:33 +0800

    init import

---
+++ src/main/kotlin/me/fanwu/GatewayDemo/GatewayDemoApplication.kt
@@ -0,0 +1,11 @@
+package me.fanwu.GatewayDemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GatewayDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<GatewayDemoApplication>(*args)
+}
---
+++ src/test/kotlin/me/fanwu/GatewayDemo/GatewayDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.fanwu.GatewayDemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GatewayDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
